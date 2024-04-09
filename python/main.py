9e3b100a8b8354e4d6c3bd9417129c9f142489ca
刘启明 <455043818@qq.com>
2024-04-09 19:27:32 +0800

    feat: whisper 直接转换为srt字幕

--- python/main.py
+++ python/main.py
@@ -1,7 +1,6 @@
 import whisper
+from whisper.utils import get_writer
 import os
-from faster_whisper import WhisperModel
-
 def fast(): 
     model_size = "large-v3"
 
@@ -11,6 +10,7 @@ def fast():
     # or run on GPU with INT8
     # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
     # or run on CPU with INT8
+    # https://github.com/openai/whisper/discussions/98
 
     current_directory = os.getcwd()
     print(current_directory)
@@ -28,9 +28,24 @@ def fast():
     for segment in segments:
         print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
 def main():
-    model = whisper.load_model("large-v3")
-    result = model.transcribe("audio.m4a", fp16 =False)
-    print(result["text"])
+
+    filename = "audio.m4a"
+    input_directory = "."
+    input_file = "{input_directory}/{filename}"
+
+
+    model = whisper.load_model("large-v3") # or whatever model you prefer
+    result = model.transcribe(audio=filename, fp16 =False)
+    output_directory = "."
+
+    word_options = {
+    "highlight_words": True,
+    "max_line_count": 50,
+    "max_line_width": 3
+    }
+
+    srt_writer = get_writer("srt", output_directory)
+    srt_writer(result, filename, word_options)
 
 if __name__ == "__main__":
-    fast()
+    main()
