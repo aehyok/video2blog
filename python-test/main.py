c3592dd7e4cd93d61f7e100d8b2ccfde933fd12e
刘启明 <455043818@qq.com>
2024-05-09 20:23:09 +0800

    fix: whisper run parameters

--- python-test/main.py
+++ python-test/main.py
@@ -5,23 +5,35 @@ import time
 import torch
 import os   #引用OS
 from faster_whisper import WhisperModel
-def fast(): 
+import whisperx
+import gc 
+
+def whisperx_test():
+    device = "cpu" 
+    model_size = "large-v3"
+    audio_file = "test.mp4"
+    batch_size = 16
+    compute_type = "int8" 
+
+    # widnow CPU
+    model = whisperx.load_model("large-v3", device, compute_type=compute_type)
+
+    # window GPU
+    # model = whisperx.load_model("large-v3", "cuda", compute_type="float16")
+
+    audio = whisperx.load_audio(audio_file)
+    result = model.transcribe(audio, batch_size=batch_size)
+
+    print(result["segments"])
+
+def faster_whisper_test(): 
     model_size = "large-v3"
 
-    # Run on GPU with FP16
-    # model = WhisperModel(model_size, device="cuda", compute_type="float16")
-
-    # or run on GPU with INT8
-    # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
-    # or run on CPU with INT8
-    # https://github.com/openai/whisper/discussions/98
-
-    # print(path)
+    ## window cpu
     model = WhisperModel(model_size, device="cpu", compute_type="int8", cpu_threads=16)
+    
+    # window gpu
     # model = WhisperModel(model_size, device="cuda", compute_type="float16")
-
-    # transcribe 语音转文字
-    # translate
     segments, info = model.transcribe("test.mp4", beam_size = 5)
 
     print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
@@ -29,13 +41,17 @@ def fast():
     for segment in segments:
         print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
 
-def main():
+def whisper_test():
 
     os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 
     filename = "test.mp4"   
 
-    model = whisper.load_model("large-v3",device="cpu")
+    ## window GPU  cuda
+    ## window CPU  cpu
+    ## mac CPU cpu
+    ## mac GPU
+    model = whisper.load_model("large-v3",device="cuda")
     result = model.transcribe(audio=filename, fp16 =False)
     output_directory = "."
 
@@ -52,7 +68,8 @@ if __name__ == "__main__":
     # print(torch.cuda.is_available())
     start_time = time.time()  # 开始时间
     print("start time:", start_time)
-    # main()
-    fast()
+    # whisper_test()
+    # faster_whisper_test()
+    whisperx_test()
     end_time = time.time()  # 结束时间
     print("Execution time: ", end_time - start_time, "seconds")
